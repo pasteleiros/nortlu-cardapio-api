24fcf30f4e9da867a0c6e782eb909714b6959200
Guilherme Vicente <guilhermedovicente@gmail.com>
2021-07-09 20:08:13 -0400

    Subindo controller e service da categoria

--- src/main/kotlin/com/nortlu/controller/CategoriaController.kt
+++ src/main/kotlin/com/nortlu/controller/CategoriaController.kt
@@ -2,8 +2,9 @@ package com.nortlu.controller
 
 import com.nortlu.service.CategoriaService
 import io.github.pasteleiros.nortlulib.dto.CategoriaDto
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.*
 import javax.inject.Inject
 
 @Controller("/categoria")
@@ -15,4 +16,13 @@ class CategoriaController (val categoriaService : CategoriaService){
 
     @Get("/listar")
     fun listarCaregorias() : List<CategoriaDto> = categoriaService.listar()
+
+    @Post
+    fun criarCategoria(@Body categoriaDto : CategoriaDto) : CategoriaDto = categoriaService.criar(categoriaDto)
+
+    @Delete("/{id}")
+    fun deletarCategoria(@PathVariable id: Long) = categoriaService.deletar(id)
+
+    @Get("/{id}")
+    fun buscarPorId(@PathVariable id: Long) : HttpResponse<CategoriaDto> = HttpResponse.ok(categoriaService.buscar(id))
 }
--- src/main/kotlin/com/nortlu/service/CategoriaService.kt
+++ src/main/kotlin/com/nortlu/service/CategoriaService.kt
@@ -2,6 +2,9 @@ package com.nortlu.service
 
 import com.nortlu.repo.CategoriaRepo
 import io.github.pasteleiros.nortlulib.dto.CategoriaDto
+import io.github.pasteleiros.nortlulib.exception.NaoEncontradoException
+import io.github.pasteleiros.nortlulib.mapper.toDto
+import io.github.pasteleiros.nortlulib.mapper.toEntity
 import io.github.pasteleiros.nortlulib.mapper.toListDto
 import javax.inject.Singleton
 
@@ -16,4 +19,10 @@ class CategoriaService (val categoriaRepo : CategoriaRepo, val redis : RedisServ
         return categorias
     }
 
+    fun criar(categoriaDto : CategoriaDto) : CategoriaDto = categoriaRepo.save(categoriaDto.toEntity()).toDto()
+
+    fun deletar(id: Long) = categoriaRepo.deleteById(id)
+
+    fun buscar(id: Long) : CategoriaDto = categoriaRepo.findById(id)
+            .orElseThrow { NaoEncontradoException("Categoria não encontrada") }.toDto()
 }
