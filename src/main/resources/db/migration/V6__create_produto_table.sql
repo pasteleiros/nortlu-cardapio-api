b8d6b025f37819d1e7e03ee1fb66d44374f16b06
Uniliva <uniliva.pereira@confitec.com.br>
2021-08-16 21:09:50 -0300

    Ajustando pedidos

--- src/main/resources/db/migration/V6__create_produto_table.sql
+++ src/main/resources/db/migration/V6__create_produto_table.sql
@@ -5,8 +5,6 @@ CREATE TABLE produto (
 	nome varchar(255) NULL,
 	valor numeric(19,2) NULL,
 	id_categoria integer NULL,
-	id_pedido integer NOT NULL,
 	CONSTRAINT pk_produto PRIMARY KEY (id),
-	CONSTRAINT fk_pedido FOREIGN KEY (id_pedido) REFERENCES pedido(id),
 	CONSTRAINT fk_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id)
 );
